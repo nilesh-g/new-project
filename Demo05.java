40213d572fd6734e0d140b3458ac18a50f62d5f6
Nilesh Ghule <nilesh@sunbeaminfo.com>
2024-07-10 16:31:16 +0530

    and program

---
+++ Demo05.java
@@ -0,0 +1,7 @@
+public class Demo05 {
+    public static void main(String[] args) {
+        int num1 = 22, num2 = 7, result;
+        result = num1 & num2;
+        System.out.println("AND: " + result);
+    }
+}
