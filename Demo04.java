91603cd08308dee297f5cbb7c04aed87cb597d8e
Nilesh Ghule <nilesh@sunbeaminfo.com>
2024-07-10 16:23:22 +0530

    modulus program

---
+++ Demo04.java
@@ -0,0 +1,7 @@
+public class Demo04 {
+    public static void main(String[] args) {
+        int num1 = 22, num2 = 7, result;
+        result = num1 % num2;
+        System.out.println("Remainder: " + result);
+    }
+}
